be9b8a6c913f60e7adde51258eeb8dc2304d7744
Giorgio Zanni <g.zanni@hotmail.com>
2019-03-01 15:01:57 -0300

    ADD: edit functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: :destroy
+  before_action :set_post, only: %i[show edit update destroy]
 
   def index
     @post = Post.new
@@ -13,6 +13,22 @@ class PostsController < ApplicationController
       respond_to :js
     else
       redirect_to root_path, alert: 'Ha ocurrido un error creando el post. Por favor inténtelo nuevamente.'
+    end
+  end
+
+  def show
+    respond_to :js
+  end
+
+  def edit
+    respond_to :js
+  end
+
+  def update
+    if @post.update(post_params)
+      respond_to :js
+    else
+      redirect_to root_path, alert: 'Hubo un problema. Por favor inténtelo de nuevo.'
     end
   end
 
