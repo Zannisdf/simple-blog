fce7a3afa5c4328cc0519ae8894db1fb19c0897c
Giorgio Zanni <g.zanni@hotmail.com>
2019-03-01 11:56:04 -0300

    ADD: create functionality by ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,22 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @post = Post.new
+    @posts = Post.order('id DESC')
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      respond_to :js
+    else
+      redirect_to root_path, alert: 'Ha ocurrido un error creando el post. Por favor inténtelo nuevamente.'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
